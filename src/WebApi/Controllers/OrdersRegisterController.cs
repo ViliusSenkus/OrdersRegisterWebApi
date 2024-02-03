0d7d7fd93564bbe0adc0baaf4a1e4a95eb947b97
ViliusSenkus <vilius.s@outlook.com>
2024-02-03 15:03:34 +0200

    All endpoints added, not documented

--- src/WebApi/Controllers/OrdersRegisterController.cs
+++ src/WebApi/Controllers/OrdersRegisterController.cs
@@ -12,24 +12,44 @@ namespace WebApi.Controllers;
 
 public class OrdersRegisterController : ControllerBase
 {
-    [HttpGet()]
+    [HttpGet(Name = "GetWeatherForecast"))]
     [Consumes(MediaTypeNames.Application.Json)]
     [SwaggerOperation(Summary = "Shows order register entry of given id",
                 Description = "Shows order registration data according given id")]
     [SwaggerResponse(StatusCodes.Status200OK, "Successfully returned order rigister entry", typeof(OrdersRegisterEntity), MediaTypeNames.Application.Json)]
     [SwaggerResponse(StatusCodes.Status500InternalServerError)]
-    public IActionResult Get([FromQuery] int regid)
+    public IActionResult GetById([FromQuery] int regid)
     {
         string response = "iskomas irasas - Nr." + regid;
         return Ok(response);
-            
-        //    Enumerable.Range(1, 5).Select(index => new OrdersRegisterEntity
-        //{
-            //Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            //TemperatureC = Random.Shared.Next(-20, 55),
-            //Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        //})
-        //.ToArray();
+    }
+
+    [HttpGet()]
+    public IActionResult GetAll()
+    {
+        string response = "Cia visas sarasas irasu ";
+        return Ok(response);
+    }
+
+    [HttpPost()]
+    public IActionResult Create([FromBody] OrdersRegisterEntity changedEntity)
+    {
+        string response = changedEntity.CountingMethod;
+        return Created("get", response);
+    }
+
+    [HttpPut()]
+    public IActionResult Update([FromQuery] int regid, [FromBody] OrdersRegisterEntity changedEntity)
+    {
+        string response = changedEntity.CountingMethod;
+        return Created("get", response);
+    }
+
+    [HttpDelete()]
+    public IActionResult delete([FromQuery] int regid)
+    {
+        string response = "iskomas irasas - Nr." + regid;
+        return NoContent();
     }
 }
 
